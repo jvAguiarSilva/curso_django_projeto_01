f70357e828f7baa7be598010466457ff3188a155
jvAguiarSilva <joaovitoraguiar@alu.ufc.br>
2022-07-30 20:33:25 -0300

    Criando apps e urls aninhadas

--- projeto/urls.py
+++ projeto/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import views
 from django.contrib import admin
-from django.urls import path
+from django.http import HttpResponse
+from django.urls import include, path
+from recipes.views import contato, home, sobre
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('recipes.urls'))
 ]
---
+++ recipes/urls.py
@@ -0,0 +1,12 @@
+from django import views
+from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import path
+
+from recipes.views import contato, home, sobre
+
+urlpatterns = [
+    path('', home),
+    path('sobre/', sobre),
+    path('contato/', contato)
+]
